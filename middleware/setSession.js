425265dffe7516cea81c60248d49bcd0d02298e2
Vu Nguyen <vunguyen10a8.nt@gmail.com>
2023-06-13 09:08:47 +0700

    bug fixed

--- middleware/setSession.js
+++ middleware/setSession.js
@@ -1,17 +1,21 @@
 const userModel = require('../model/userModel');
 
 const setSession = (req, res, next) => {
-    if(req.cookies.remember){
-        userModel.getUserByToken(req.cookies.remember, (err, user) => {
-            if (err) throw  err;
-            req.session.user = {
-                maKH: user.maKH,
-                username: user.userName,
-                role: user.role
-            };
-        })
-    } 
-    next();
+    try {
+        if(req.cookies.remember){
+            userModel.getUserByToken(req.cookies.remember, (err, user) => {
+                if (err) throw  err;
+                req.session.user = {
+                    maKH: user.maKH,
+                    username: user.userName,
+                    role: user.role
+                };
+            })
+        } 
+        next();
+    } catch (err) {
+        res.redirect('/logout');
+    }
 }
 
 module.exports = setSession
